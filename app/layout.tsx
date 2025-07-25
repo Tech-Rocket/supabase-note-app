b98c16d9428b1e044d9a510666c3fb640dd2defb
Tech-Rocket <beelawrence8@gmail.com>
2025-07-25 14:17:59 +0000

    Setup the sonner toaster within the layout.tsx file

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { ThemeProvider } from "@/components/providers/theme-provider";
+import { Toaster } from "@/components/ui/sonner";
 
 export const metadata: Metadata = {
   title: "Supabase Note App",
@@ -21,6 +22,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
+          <Toaster />
         </ThemeProvider>
       </body>
     </html>
