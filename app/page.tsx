70b46297feedee94df03f4e4986f1721822dcd9f
Tech-Rocket <beelawrence8@gmail.com>
2025-08-05 20:21:13 +0000

    feat: implement task submission functionality; update supabase client configuration

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,38 @@
+"use client";
+
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import { supabase } from "./supabase-client";
 
 export default function HomePage() {
+  const [newTask, setNewTask] = useState({ title: "", description: "" });
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+
+    const { error } = await supabase.from("tasks").insert(newTask).single();
+
+    if (error) {
+      console.log("Failed to add the tasks", error.message);
+    }
+    setNewTask({ title: "", description: "" });
+  };
   return (
     <section className="flex flex-col items-center justify-center">
       <h2 className="bold mb-5 text-center text-3xl">Task Manager CRUD</h2>
       <div className="text-centerw-[35rem] w-2xl">
         <form
-          action=""
+          onSubmit={handleSubmit}
           className="flex flex-col items-center justify-center space-y-5"
         >
           <Input
             type="text"
             placeholder="Tast title"
+            required
+            onChange={(e) =>
+              setNewTask((prev) => ({ ...prev, title: e.target.value }))
+            }
             className="h-12 w-full rounded border border-gray-300"
           />
 
@@ -20,6 +40,10 @@ export default function HomePage() {
             placeholder="Task Description"
             id=""
             cols={4}
+            required
+            onChange={(e) =>
+              setNewTask((prev) => ({ ...prev, description: e.target.value }))
+            }
             className="w-full rounded border border-gray-300 p-3"
           ></textarea>
           <Button className="h-12 w-[12rem] cursor-pointer rounded text-sm font-semibold">
--- app/supabase-client.ts
+++ app/supabase-client.ts
@@ -1,6 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabase_url = process.env.SUPABASE_URL!;
-const supabase_anon_key = process.env.SUPABASE_ANON_KEY!;
+// const supabaseUrl = process.env.SUPABASE_URL!;
+// const supabase_anon_key = process.env.SUPABASE_ANON_KEY!;
 
-export const supabase = createClient(supabase_url, supabase_anon_key);
+export const supabase = createClient(
+  "https://ecrsooxvgriplornrrph.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVjcnNvb3h2Z3JpcGxvcm5ycnBoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI1MDE0NjcsImV4cCI6MjA2ODA3NzQ2N30.DsenZ1_Y756XDCKiQbVSikbCG1utzelv_kp91bHuaZg",
+);
