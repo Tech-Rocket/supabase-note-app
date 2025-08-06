2b34b954521188478e528fcb4a127cbcf196c7bb
Tech-Rocket <beelawrence8@gmail.com>
2025-08-06 12:56:29 +0000

    feat: enhance task management functionality; implement task fetching, updating, and deletion

--- app/page.tsx
+++ app/page.tsx
@@ -2,22 +2,68 @@
 
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { supabase } from "./supabase-client";
 
+interface TaskListTypes {
+  id: number;
+  title: string;
+  description: string;
+  created_at: string;
+}
 export default function HomePage() {
   const [newTask, setNewTask] = useState({ title: "", description: "" });
+  const [updateTaskDescription, setUpdateTaskDescription] = useState("");
+  const [taskList, setTasklist] = useState<TaskListTypes[]>([]);
+
+  const fetchTasks = async () => {
+    const { error, data } = await supabase
+      .from("tasks")
+      .select("*")
+      .order("created_at", { ascending: true });
+    if (error) {
+      console.log("Failed to tasks data", error.message);
+      return;
+    }
+    setTasklist(data);
+  };
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
 
   const handleSubmit = async (e: any) => {
     e.preventDefault();
-
     const { error } = await supabase.from("tasks").insert(newTask).single();
-
     if (error) {
       console.log("Failed to add the tasks", error.message);
+      return;
     }
     setNewTask({ title: "", description: "" });
+    alert("New tasks added successfully");
   };
+
+  const handleDeleteTask = async (id: number) => {
+    const { error } = await supabase.from("tasks").delete().eq("id", id);
+    if (error) {
+      console.log("Error deleting tasks", error.message);
+      return;
+    }
+    alert("Tasks deleted successfully");
+  };
+
+  const handleUpdateTask = async (id: number) => {
+    const { error } = await supabase
+      .from("tasks")
+      .update({ description: updateTaskDescription })
+      .eq("id", id);
+    if (error) {
+      console.log("Error updating tasks", error.message);
+      return;
+    }
+    alert("Tasks updated successfully");
+  };
+
   return (
     <section className="flex flex-col items-center justify-center">
       <h2 className="bold mb-5 text-center text-3xl">Task Manager CRUD</h2>
@@ -51,19 +97,35 @@ export default function HomePage() {
           </Button>
         </form>
 
-        <div className="mt-8 flex w-full flex-col items-center justify-center space-y-3 rounded-sm border border-gray-100 py-8 shadow">
-          <h2 className="text-2xl font-semibold">Title</h2>
-          <p className="text-lg text-gray-400">Description</p>
+        {taskList.map((task) => (
+          <div
+            key={task.id}
+            className="mt-4 flex w-full flex-col items-center justify-center space-y-3 rounded-sm border border-gray-100 py-8 shadow"
+          >
+            <h2 className="text-2xl font-semibold">{task.title}</h2>
+            <p className="text-lg text-gray-400">{task.description}</p>
 
-          <div className="space-x-3">
-            <Button className="h-12 w-[12rem] cursor-pointer rounded text-sm font-semibold">
-              Edit
-            </Button>
-            <Button className="h-12 w-[12rem] cursor-pointer rounded border-none bg-red-700 text-sm font-semibold text-white hover:text-black">
-              Delete
-            </Button>
+            <div className="space-x-3">
+              <textarea
+                onChange={(e) => setUpdateTaskDescription(e.target.value)}
+                className="m-3 w-full rounded border border-gray-300 p-3"
+                placeholder="updated description..."
+              />
+              <Button
+                onClick={() => handleUpdateTask(task.id)}
+                className="h-12 w-[12rem] cursor-pointer rounded text-sm font-semibold"
+              >
+                Edit
+              </Button>
+              <Button
+                onClick={() => handleDeleteTask(task.id)}
+                className="h-12 w-[12rem] cursor-pointer rounded border-none bg-red-700 text-sm font-semibold text-white hover:text-black"
+              >
+                Delete
+              </Button>
+            </div>
           </div>
-        </div>
+        ))}
       </div>
     </section>
   );
--- components/shared/auth-form.tsx
+++ components/shared/auth-form.tsx
@@ -4,20 +4,22 @@ import { useRouter } from "next/navigation";
 import { CardContent, CardFooter } from "../ui/card";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
-import { useTransition } from "react";
+import { ChangeEvent, useActionState, useTransition } from "react";
 import { Button } from "@/components/ui/button";
 import { Loader2 } from "lucide-react";
+import { useState } from "react";
 import Link from "next/link";
+import { supabase } from "@/app/supabase-client";
 
 interface AuthFormTypes {
   type: "login" | "register";
 }
 
 export default function AuthForm(props: AuthFormTypes) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const isLoginForm = props.type === "login";
   const router = useRouter();
-
-  const handleSubmit = (formData: FormData) => {};
 
   const [isPending, startTranstion] = useTransition();
 
@@ -31,6 +33,27 @@ export default function AuthForm(props: AuthFormTypes) {
 
   const linkText = isLoginForm ? "Sign up" : "Login";
 
+  const handleSubmit = async (formData: FormData) => {
+    if (isLoginForm) {
+      const { error: signInError } = await supabase.auth.signInWithPassword({
+        email,
+        password,
+      });
+      if (signInError) {
+        console.log("Error loggin", signInError.message);
+        return;
+      }
+    } else {
+      const { error: signUpError } = await supabase.auth.signUp({
+        email,
+        password,
+      });
+      if (signUpError) {
+        console.log("Error signing up", signUpError.message);
+        return;
+      }
+    }
+  };
   return (
     <>
       <form action={handleSubmit}>
@@ -43,10 +66,14 @@ export default function AuthForm(props: AuthFormTypes) {
               id="email"
               type="email"
               name="email"
+              value={email}
               placeholder="example@gmail.com"
               required
               disabled={isPending}
               className="h-12 rounded"
+              onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                setEmail(e.target.value)
+              }
             />
           </div>
           <div className="flex flex-col space-y-1">
